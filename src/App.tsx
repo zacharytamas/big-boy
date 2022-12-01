7ef4b8546afa72370378549d9ff46e60a57749d9
Zachary Jones <zachary@jones.cx>
2022-12-01 12:38:38 -0500

    Add Tailwind and apply some basic UI improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
-import './App.css';
+import cn from 'classnames';
 import { Howl } from 'howler';
 
 import { useChuffing } from './sound/useChuffing';
@@ -8,6 +8,7 @@ import { useSpeed } from './streams/useSpeed';
 function App() {
   const [speed, setSpeed] = useState<number>(0);
 
+  const [engineOn, setEngineOn] = useState(false);
   const [bellActive, setBellActive] = useState(false);
 
   const chuffing = useChuffing();
@@ -16,6 +17,14 @@ function App() {
   const bellSound = useMemo(() => {
     return new Howl({ src: 'bell.mp3', loop: true });
   }, []);
+
+  useEffect(() => {
+    if (engineOn) {
+      chuffing.start();
+    } else {
+      chuffing.stop();
+    }
+  }, [engineOn, chuffing]);
 
   useEffect(() => {
     speed$.subscribe((speed_) => {
@@ -37,40 +46,37 @@ function App() {
   }, [bellActive, bellSound]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button
-          onClick={() => {
-            setBellActive(true);
-          }}
-        >
-          Start Bell
-        </button>
-        <button
-          onClick={() => {
-            setBellActive(false);
-          }}
-        >
-          Stop Bell
-        </button>
-        <button
-          onClick={() => {
-            chuffing.start();
-          }}
-        >
-          Start chuffing
-        </button>
-        <button
-          onClick={() => {
-            chuffing.stop();
-          }}
-        >
-          Stop chuffing
-        </button>
+    <div className="p-4">
+      <div className="flex flex-col space-y-4">
+        <div className="flex border-black rounded-lg border-4 p-4 text-2xl">
+          <div className="flex-1 font-bold">SPEED</div>
+          <div className="flex-1">{`${(speed * 2.237).toFixed(0)} MPH`}</div>
+        </div>
 
-        <h1>Speed</h1>
-        <p>{`${speed} m/s`}</p>
-      </header>
+        <div
+          className={cn('flex border rounded-lg p-4 items-center', {
+            'bg-green-100 border-green-500': engineOn,
+          })}
+          onClick={() => {
+            setEngineOn((val) => !val);
+          }}
+        >
+          <div className="flex-1 font-bold uppercase text-2xl">Engine</div>
+          <div className="flex-1 text-lg">{engineOn ? `Running` : `Not Running`}</div>
+        </div>
+
+        <div
+          className={cn('flex border rounded-lg p-4 items-center', {
+            'bg-green-100 border-green-500': bellActive,
+          })}
+          onClick={() => {
+            setBellActive((val) => !val);
+          }}
+        >
+          <div className="flex-1 font-bold uppercase text-2xl">Bell</div>
+          <div className="flex-1 text-lg">{bellActive ? `Ringing` : `Not Ringing`}</div>
+        </div>
+      </div>
     </div>
   );
 }
