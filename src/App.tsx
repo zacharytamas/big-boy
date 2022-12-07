88543f9c1596823915c3ba44015fd16e9b553811
Zachary Jones <zachary@jones.cx>
2022-12-07 10:47:22 -0500

    feat: Set a max width on the UI so it doesn't look silly on large screens

--- src/App.tsx
+++ src/App.tsx
@@ -35,46 +35,60 @@ function App() {
   }, [engineOn, chuffing]);
 
   return (
-    <div className="p-4">
-      <div className="flex flex-col space-y-4">
-        <SpeedControlPanel
-          speedMachine={speedMachine_}
-          decreaseSpeed={() => sendSpeedMachine('MANUAL_SPEED_DECREASE')}
-          increaseSpeed={() => sendSpeedMachine('MANUAL_SPEED_INCREASE')}
-          toggleAuto={() => {
-            sendSpeedMachine(speedMachine_.matches('auto') ? 'START_STATIC' : 'START_AUTO');
-          }}
-        />
+    <div className="mx-auto max-w-xl">
+      <div className="p-4">
+        <div className="flex flex-col space-y-4">
+          <SpeedControlPanel
+            speedMachine={speedMachine_}
+            decreaseSpeed={() => sendSpeedMachine('MANUAL_SPEED_DECREASE')}
+            increaseSpeed={() => sendSpeedMachine('MANUAL_SPEED_INCREASE')}
+            toggleAuto={() => {
+              sendSpeedMachine(
+                speedMachine_.matches('auto') ? 'START_STATIC' : 'START_AUTO'
+              );
+            }}
+          />
 
-        <div
-          className={cn('flex border rounded-lg p-4 items-center cursor-pointer', {
-            'bg-green-100 border-green-500': engineOn,
-          })}
-          onClick={() => {
-            setEngineOn((val) => !val);
-          }}
-        >
-          <div className="flex-1 font-bold uppercase text-2xl">Engine</div>
-          <div className="flex-1 text-lg">{engineOn ? `Running` : `Not Running`}</div>
+          <div
+            className={cn(
+              'flex cursor-pointer items-center rounded-lg border p-4',
+              {
+                'border-green-500 bg-green-100': engineOn,
+              }
+            )}
+            onClick={() => {
+              setEngineOn((val) => !val);
+            }}
+          >
+            <div className="flex-1 text-2xl font-bold uppercase">Engine</div>
+            <div className="flex-1 text-lg">
+              {engineOn ? `Running` : `Not Running`}
+            </div>
+          </div>
+
+          <div
+            className={cn(
+              'flex cursor-pointer items-center rounded-lg border p-4',
+              {
+                'border-green-500 bg-green-100': bellIsRinging,
+              }
+            )}
+            onClick={() => {
+              sendBellMachine(bellIsRinging ? `STOP` : `RING`);
+            }}
+          >
+            <div className="flex-1 text-2xl font-bold uppercase">Bell</div>
+            <div className="flex-1 text-lg">
+              {bellIsRinging ? `Ringing` : `Not Ringing`}
+            </div>
+          </div>
+
+          <WhistleButton
+            sounding={whistleMachine_.matches('sounding')}
+            startSounding={() => sendWhistleMachine('START')}
+            stopSounding={() => sendWhistleMachine('STOP')}
+          />
         </div>
-
-        <div
-          className={cn('flex border rounded-lg p-4 items-center cursor-pointer', {
-            'bg-green-100 border-green-500': bellIsRinging,
-          })}
-          onClick={() => {
-            sendBellMachine(bellIsRinging ? `STOP` : `RING`);
-          }}
-        >
-          <div className="flex-1 font-bold uppercase text-2xl">Bell</div>
-          <div className="flex-1 text-lg">{bellIsRinging ? `Ringing` : `Not Ringing`}</div>
-        </div>
-
-        <WhistleButton
-          sounding={whistleMachine_.matches('sounding')}
-          startSounding={() => sendWhistleMachine('START')}
-          stopSounding={() => sendWhistleMachine('STOP')}
-        />
       </div>
     </div>
   );
